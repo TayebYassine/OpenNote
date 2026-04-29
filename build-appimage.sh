2990605e8502738b5e430fa35a0a044e4a556f1d
TayebYassine <TayebYassine.Fekih-Ahmed@esprit.tn>
2026-04-29 15:57:17 +0100

    updated version

--- build-appimage.sh
+++ build-appimage.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 QT_DIR="$HOME/Qt/6.7.3/gcc_64"
 BUILD_DIR="build-appimage"
-APP_VERSION="1.1.0"
+APP_VERSION="1.2.0"
 
 [ ! -f "packaging/opennote.desktop" ] && echo "ERROR: packaging/opennote.desktop not found." && exit 1
 
